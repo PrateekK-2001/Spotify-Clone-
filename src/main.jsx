b75db8270391c8cef8b40509e180d05250fed7f0
Prateek Kumar Patel <153807112+PrateekK-2001@users.noreply.github.com>
2024-09-20 12:19:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'// imfort for use the react router dom
+import { Hashimport { HashRouter as Router } from 'react-router-dom';
 import PlayerContextProvider from './context/PlayerContext.jsx'
 
 createRoot(document.getElementById('root')).render(
